ac3d83219eba8cd39d85384ff2a0bf184612b330
rainTai <13612260854@163.com>
2023-01-12 13:50:53 +0800

    :sparkles: feat(component): 111

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
-module.exports = {
+// @ts-check
+const { defineConfig } = require('eslint-define-config')
+module.exports = defineConfig({
   root: true,
   env: {
     browser: true,
@@ -18,10 +20,11 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'plugin:prettier/recommended',
   ],
   rules: {
-    'vue/script-setup-uses-vars': 'off',
+    'vue/script-setup-uses-vars': 'error',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
@@ -51,6 +54,8 @@ module.exports = {
     'space-before-function-paren': 'off',
 
     'vue/attributes-order': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/multi-word-component-names': 'off',
     'vue/one-component-per-file': 'off',
     'vue/html-closing-bracket-newline': 'off',
     'vue/max-attributes-per-line': 'off',
@@ -58,7 +63,6 @@ module.exports = {
     'vue/singleline-html-element-content-newline': 'off',
     'vue/attribute-hyphenation': 'off',
     'vue/require-default-prop': 'off',
-    'vue/require-explicit-emits': 'off',
     'vue/html-self-closing': [
       'error',
       {
@@ -71,7 +75,5 @@ module.exports = {
         math: 'always',
       },
     ],
-    'vue/multi-word-component-names': 'off',
-    'vue/no-v-html': 'off',
   },
-}
+})
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,10 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <p>sss Edit <code>components/HelloWorld.vue</code> to22222 test HMR</p>
   </div>
 
   <p>
@@ -27,7 +24,7 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/vossslar" target="_blank">Volar</a>
     in your IDE{{ 'zbcd' + 'ssss' + 1 }
   </p>
-  ssss 1234asdfs{ss
+  ssss!! ssss 1234asdfs{ss
   <p class="read-the-docs">Click on the Vite and Vue logos to lear12341n more</p>
 </template>
 
