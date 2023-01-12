628cb4327b200fed36c342678d0d1e5059307610
rainTai <13612260854@163.com>
2023-01-12 11:31:49 +0800

    :sparkles: feat(component): :sparkles: feat(component): 测试webhooks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,14 +19,13 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
+    official Vue + Vite starter
   </p>
   <p>
     Install
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
+    in your IDE for a
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
